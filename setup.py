84e576567ce628b81de6a2ac6fa938191e66cb14
Paul Norman <penorman@mac.com>
2020-04-21 06:30:10 -0700

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tilekiln',
-    version='0.0.2',
+    version='0.0.3',
     author="Paul Norman",
     author_email="osm@paulnorman.ca",
     url="https://github.com/pnorman/tilekiln",
